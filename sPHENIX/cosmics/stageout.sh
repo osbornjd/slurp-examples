aeab4e7ab7e85a620e745ef8ac84580911035d70
klendathu2k <jwebb@bnl.gov>
2024-03-05 10:44:57 -0500

    use cups for stageout

--- sPHENIX/cosmics/stageout.sh
+++ sPHENIX/cosmics/stageout.sh
@@ -18,28 +18,17 @@ segment=`echo ${base} | cut -d'-' -f3`
 
 echo stageout ${filename} ${destination} start `date`
 
-#./cups.py -r ${runnumber} -s ${segment} -d test stageout ${filename} ${destination}
+./cups.py -r ${runnumber} -s ${segment} -d test stageout ${filename} ${destination}
 
-#md5true=(`md5sum ${filename}`)
-cp --verbose  ${filename} ${destination}  
-#md5check=(`md5sum ${destination}/${filename}`)
-
-#echo stageout ${filename} ${destination} copied `date`
+#cp --verbose  ${filename} ${destination}  
 
 ./cups.py -r ${runnumber} -s ${segment} -d DST_TPCCOSMICS_${build}_${dbtag} catalog --ext root --path ${destination} --dataset ${build}_${dbtag} --hostname lustre --nevents 0
-#$$$./cups.py -r ${runnumber} -s ${segment} -d DST_TPCCOSMICS catalog --ext root --path ${destination} --dataset test --hostname lustre --nevents 0
 
 # Once it has been moved... and perhaps we place some error checking here... we remove it.
 
 echo Remove ${filename} `date`
 rm ${filename}
 
-#dbtag=${dstname##*_}
-#echo ${dbtag}
 
-#echo ${base#*-}      # run-segment
-#runnumber_segment=${base#*-}   
-#segment=${runnumber_segment#*-}
-#runnumber=${runnumber_segment%-*}
 
 
