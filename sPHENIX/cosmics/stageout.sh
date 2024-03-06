55ffe1568c50d2d6a9faf51f651cd9c51027f978
klendathu2k <jwebb@bnl.gov>
2024-03-05 19:47:33 -0500

    No more catalog... just execute cups with stageout command.

--- sPHENIX/cosmics/stageout.sh
+++ sPHENIX/cosmics/stageout.sh
@@ -3,11 +3,9 @@
 filename=${1}
 destination=${2}
 
-# decode filename
-#echo ${filename}         # filename
-#echo ${filename/.root/}  # strip suffix
-#echo ${filename%-*}      # strip segment
+echo stageout ${filename} ${destination} start `date`
 
+# decode filename
 base=${filename/.root/}
 dstname=${base%%-*}
 dsttype=`echo ${dstname} | cut -d'_' -f1,2`
@@ -16,18 +14,9 @@ dbtag=`echo ${dstname} | cut -d'_' -f4`
 runnumber=`echo ${base} | cut -d'-' -f2`
 segment=`echo ${base} | cut -d'-' -f3`
 
-echo stageout ${filename} ${destination} start `date`
-
 ./cups.py -r ${runnumber} -s ${segment} -d test stageout ${filename} ${destination}
 
-#cp --verbose  ${filename} ${destination}  
-
-./cups.py -r ${runnumber} -s ${segment} -d DST_TPCCOSMICS_${build}_${dbtag} catalog --ext root --path ${destination} --dataset ${build}_${dbtag} --hostname lustre --nevents 0
-
-# Once it has been moved... and perhaps we place some error checking here... we remove it.
-
-#echo Remove ${filename} `date`
-#rm ${filename}
+echo stageout ${filename} ${destination} finish `date`
 
 
 
