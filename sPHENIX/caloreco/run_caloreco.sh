542da990e85e1a335ec872f9a79e2df57ec54cf0
klendathu2k <jwebb@bnl.gov>
2024-03-08 14:54:10 -0500

    Code cleanups, and get dstname right for cups.

--- sPHENIX/caloreco/run_caloreco.sh
+++ sPHENIX/caloreco/run_caloreco.sh
@@ -5,36 +5,22 @@
 export USER="$(id -u -n)"
 export LOGNAME=${USER}
 export HOME=/sphenix/u/${USER}
-export ODBCINI=./odbc.ini
-
 hostname
 
-this_script=$BASH_SOURCE
-this_script=`readlink -f $this_script`
-this_dir=`dirname $this_script`
-echo rsyncing from $this_dir
-echo running: $this_script $*
+#this_script=$BASH_SOURCE
+#this_script=`readlink -f $this_script`
+#this_dir=`dirname $this_script`
+#echo rsyncing from $this_dir
+#echo running: $this_script $*
 
+# This is actually going to be a pfn...
 lfn=${10}
 
-source /opt/sphenix/core/bin/sphenix_setup.sh -n $8
+echo source /opt/sphenix/core/bin/sphenix_setup.sh -n ${8}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ${8}
 
-cups.py -t production_status -r $2 -s $3 --dstname=DST_CALOR_auau23_${8/./}_${9} started
+export ODBCINI=./odbc.ini
 
-if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
-then
-    cd $_CONDOR_SCRATCH_DIR
-    rsync -av $this_dir/* .
-    getinputfiles.pl ${lfn}
-    if [ $? -ne 0 ]
-    then
-	echo error from getinputfiles.pl $lfn, exiting
-	exit -1
-    fi
-else
-    echo condor scratch NOT set
-    exit 1
-fi
 # arguments 
 # $1: number of events
 # $2: run number
@@ -59,25 +45,30 @@ echo arg8 \(build tag\): $8
 echo arg9 \(cdb tag\): $9
 echo arg10 \(inputs\): ${10}
 
-ls -la
+#$$$ls -la
+
+outputfile=${6}
+dstfile=${6%.*}
+dstname=${dstfile%%-*}
+
+
+
+echo cups.py -v -t production_status -r $2 -s $3 --dstname=${dstname} started
+     cups.py -v -t production_status -r $2 -s $3 --dstname=${dstname} started
 
 echo running root.exe -q -b Fun4All_Year1.C\($1,\"${lfn}\",\"$6\",\"$7\",\"$9\"\) 
 
-cups.py -t production_status -r $2 -s $3 --dstname=DST_CALOR_auau23_${8/./}_${9} running
-root.exe -q -b  Fun4All_Year1.C\($1,\"$lfn\",\"$6\",\"$7\",\"$9\"\) | pull.py  
+echo cups.py -t production_status -r $2 -s $3 --dstname=${dstname} running
+     cups.py -v -t production_status -r $2 -s $3 --dstname=${dstname} running
 
-cups.py -t production_status -r $2 -s $3 --dstname=DST_CALOR_auau23_${8/./}_${9} exitcode --exit $?
+root.exe -q -b  Fun4All_Year1.C\($1,\"$lfn\",\"$6\",\"$7\",\"$9\"\)
 
+echo cups.py -t production_status -r $2 -s $3 --dstname=${dstname} exitcode --exit $?
+     cups.py -v -t production_status -r $2 -s $3 --dstname=${dstname} exitcode --exit $?
 
-
-# "$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).out,$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).err"
 runnumber=$(printf "%08d" $2)
 sequence=$(printf "%04d" $3)
-logbase=DST_CALOR_auau23_${8/./}_${9}-${runnumber}-${sequence}
-
-echo $runnumber
-echo $sequence
-echo $logbase
+logbase=${dstfile}
 
 #cat stdout.log
 
@@ -94,11 +85,13 @@ EOF
 
 #./cups.py -r ${2} -s ${3} -d DST_CALOR_auau23_${build}_${dbtag} quality --qafile qatest.json
 
-cp stdout.log ${logbase}.out
-cp stderr.log ${logbase}.err
+touch stderr.log
+cp stdout.log ${dstfile}.out
+cp stderr.log ${dstfile}.err
 
 ls -la
 
-} >stdout.log 2>stderr.log
+} >&stdout.log 
+#2>stderr.log
 
 echo "script done"
