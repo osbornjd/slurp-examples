66dd842cbf065ab941479594912ae967c54b2eb1
klendathu2k <jwebb@bnl.gov>
2024-03-24 13:35:15 -0400

    Update stageout script for calorimeter event builder to return nevents processed.

--- sPHENIX/eventcombine/stageout.sh
+++ sPHENIX/eventcombine/stageout.sh
@@ -14,8 +14,11 @@ dbtag=`echo ${dstname} | cut -d'_' -f4`
 runnumber=`echo ${base} | cut -d'-' -f2`
 segment=`echo ${base} | cut -d'-' -f3`
 
-echo ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} 
-     ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} 
+#            sh.dpipe( procline, w=0,s='f',d='n',i=True, _out=counter )    
+nevents=$( dpipe -w 0 -s f -d n -i ${filename} )
+
+echo ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag}  --nevents ${nevents}
+     ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag}  --nevents ${nevents}
 
 
 echo stageout ${filename} ${destination} finish `date`
