90ee3f4eca14f50fb02778dd5818bcd8ac2981c2
klendathu2k <jwebb@bnl.gov>
2024-03-30 08:57:36 -0400

    fix

--- sPHENIX/cosmics/stageout.sh
+++ sPHENIX/cosmics/stageout.sh
@@ -14,7 +14,9 @@ dbtag=`echo ${dstname} | cut -d'_' -f5`
 runnumber=`echo ${base} | cut -d'-' -f2`
 segment=`echo ${base} | cut -d'-' -f3`
 
-nevents=$( root.exe -q -b GetEntries.C\(\"${filename}\"\) | awk 'if $0 ~/Number of Entries/{ print $4; } else { print "-1"; }' )
+
+nevents_=$( root.exe -q -b GetEntries.C\(\"${filename}\"\) | awk '/Number of Entries/{ print $4; }' )
+nevents=${nevents_:--1}
 
 echo ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents}
      ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents}
