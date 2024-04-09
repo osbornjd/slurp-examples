6b087e098d4e87996a2c6fd2b3f9349086a1643f
klendathu2k <jwebb@bnl.gov>
2024-04-08 23:42:59 -0400

    Production type is needed to signal to cups whether it is updating production status at the segment of the input file (in a 1:1 prod) or segment 0 (of a many:many or 1:many prod).

--- sPHENIX/cosmics/stageout.sh
+++ sPHENIX/cosmics/stageout.sh
@@ -18,8 +18,9 @@ segment=`echo ${base} | cut -d'-' -f3`
 nevents_=$( root.exe -q -b GetEntries.C\(\"${filename}\"\) | awk '/Number of Entries/{ print $4; }' )
 nevents=${nevents_:--1}
 
-echo ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents} --inc
-     ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents} --inc
+# prodtype is required... specifies whether the production status entry manages a single output file (only) or many output files (many).
+echo ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents} --inc --prodtype many
+     ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents} --inc --prodtype many
 
 
 echo stageout ${filename} ${destination} finish `date`
