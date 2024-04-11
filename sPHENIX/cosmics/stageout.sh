dbc2bcc93f4d01d1cb42f6663fc95da39a03faf8
klendathu2k <jwebb@bnl.gov>
2024-04-11 12:22:46 -0400

    Implement (more) general filename convention.

--- sPHENIX/cosmics/stageout.sh
+++ sPHENIX/cosmics/stageout.sh
@@ -5,15 +5,41 @@ destination=${2}
 
 echo stageout ${filename} ${destination} start `date`
 
+regex_dsttype_run="(DST_[A-Z_]+[a-z]+)_([a-z]+)_(202[345]p[0-9][0-9][0-9])-([0-9]+)-([0-9]+)"
+regex_dsttype_range="(DST_[A-Z_]+[a-z]+)_([a-z]+)_(202[345]p[0-9][0-9][0-9])-([0-9]+)-([0-9]+)-([0-9]+)"
+
 # decode filename
 base=${filename/.root/}
 dstname=${base%%-*}
-dsttype=`echo ${dstname} | cut -d'_' -f1-3`
-build=`echo ${dstname} | cut -d'_' -f4`
-dbtag=`echo ${dstname} | cut -d'_' -f5`
-runnumber=`echo ${base} | cut -d'-' -f2`
-segment=`echo ${base} | cut -d'-' -f3`
 
+# Filename matches a dsttype with a single run
+if [[ $base =~ $regex_dsttype_run ]]; then
+   dsttype=${BASH_REMATCH[1]}
+ echo $dsttype ...
+   build=${BASH_REMATCH[2]}
+ echo $build
+   dbtag=${BASH_REMATCH[3]}
+ echo $dbtag
+   runnumber=${BASH_REMATCH[4]}
+ echo $runnumber
+   segment=${BASH_REMATCH[5]}
+ echo $segment
+fi
+# Filename matches a dst "run range" type
+if [[ $base =~ $regex_dsttype_range ]]; then
+   dsttype=${BASH_REMATCH[1]}
+ echo $dsttype ...
+   build=${BASH_REMATCH[2]}
+ echo $build
+   dbtag=${BASH_REMATCH[3]}
+ echo $dbtag
+   runnumber=${BASH_REMATCH[4]}
+ echo $runnumber
+   runnumber2=${BASH_REMATCH[5]}
+ echo $runnumber
+   segment=${BASH_REMATCH[6]}
+ echo $segment
+fi
 
 nevents_=$( root.exe -q -b GetEntries.C\(\"${filename}\"\) | awk '/Number of Entries/{ print $4; }' )
 nevents=${nevents_:--1}
