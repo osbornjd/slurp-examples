234ba3e15917276bf86bd4c51d01e6d65d80957f
bkimelman <benjamin.kimelman@vanderbilt.edu>
2024-08-22 10:24:07 -0400

    Added laser clustering to job0

--- sPHENIX/TrackingProduction/Fun4All_Job0.C
+++ sPHENIX/TrackingProduction/Fun4All_Job0.C
@@ -93,6 +93,8 @@ void Fun4All_Job0(
   tpcclusterizer->set_rawdata_reco();
   se->registerSubsystem(tpcclusterizer);
 
+  TPC_LaserClustering();
+
   Micromegas_Clustering();
 
   se->registerSubsystem(new MvtxClusterQA);
@@ -107,6 +109,10 @@ void Fun4All_Job0(
   out->AddNode("EventHeader");
   out->AddNode("TRKR_CLUSTER");
   out->AddNode("TRKR_CLUSTERCROSSINGASSOC");
+  if(G4TPC::ENABLE_CENTRAL_MEMBRANE_HITS)
+  {
+    out->AddNode("LASER_CLUSTER");
+  }
   se->registerOutputManager(out);
 
   se->run(nEvents);
